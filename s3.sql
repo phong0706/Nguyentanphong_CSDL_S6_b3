e0d73f4956fd71c9bb9c5a4323aa3ed4d3cfa08e
Phong <87291789+phong0706@users.noreply.github.com>
2026-04-21 21:10:38 +0700

    Add files via upload

--- s3.sql
+++ s3.sql
@@ -1,28 +1,31 @@
--- 1. Thiết kế I/O & Luồng xử lý (Tầng Backend)
--- Đầu vào (Input) & Đầu ra (Output)
--- Input: minTrustScore (tham số cấu hình từ Admin), locationOrder (vị trí đơn hàng).
--- Output: Danh sách các tài xế thỏa mãn điều kiện, được sắp xếp theo độ ưu tiên.
--- Luồng xử lý Logic (Pseudo-code):
--- Nếu Admin nhập số âm, hệ thống sẽ gặp lỗi logic nghiêm trọng (cho phép tài xế điểm cực thấp nhận đơn). Chúng ta cần xử lý như sau:
--- 1. Nhận giá trị minTrustScore từ hệ thống.
--- 2. Kiểm tra điều kiện:
--- + Nếu minTrustScore < 0:
--- + Gán giá trị mặc định là 80 (theo yêu cầu nghiệp vụ cơ bản).
--- + (Hoặc) Trả về lỗi yêu cầu Admin nhập lại giá trị hợp lệ.
--- 3. Xác thực: Đảm bảo minTrustScore luôn là số dương trước khi truyền vào câu lệnh SQL.
--- 4.Gọi SQL với tham số đã được làm sạch.
+-- 1. Thiết kế I/O & Ý tưởng Logic
+-- Ý tưởng "Đánh dấu và Cộng"
+-- Để đếm riêng số đơn bị hủy (CANCELLED) mà không làm ảnh hưởng đến tổng số đơn, chúng ta sử dụng kỹ thuật biến đổi dữ liệu trước khi đưa vào hàm tổng hợp:
+-- Bước 1: Dùng CASE WHEN để quét từng dòng. Nếu status = 'CANCELLED', ta gán cho nó giá trị là 1. Ngược lại, gán giá trị là 0.
+-- Bước 2: Dùng hàm SUM() để cộng tất cả các số 1 và 0 đó lại.
+-- Kết quả: Tổng các số 1 chính là số lượng đơn bị hủy của tài xế đó.
+-- Luồng xử lý
+-- Gom nhóm: GROUP BY user_id.
+-- Tính toán:
+-- total_bookings: Dùng COUNT(*).
+-- cancelled_bookings: Dùng SUM(CASE WHEN ... THEN 1 ELSE 0 END).
+-- Lọc kết quả: Vì điều kiện nằm trên kết quả sau khi tính toán (Aggregate), ta bắt buộc phải dùng HAVING.
+
+-- Luồng xử lý
+-- 1 .Gom nhóm: GROUP BY user_id.
+-- 2. Tính toán:
+-- - total_bookings: Dùng COUNT(*).
+-- - cancelled_bookings: Dùng SUM(CASE WHEN ... THEN 1 ELSE 0 END).
+-- 3. Lọc kết quả: Vì điều kiện nằm trên kết quả sau khi tính toán (Aggregate), ta bắt buộc phải dùng HAVING.
 
 SELECT 
-    driver_id, 
-    driver_name, 
-    status, 
-    trust_score, 
-    distance_km
+    user_id,
+    COUNT(*) AS total_bookings,
+    SUM(CASE WHEN status = 'CANCELLED' THEN 1 ELSE 0 END) AS cancelled_bookings
 FROM 
-    Drivers
-WHERE 
-    status = 'AVAILABLE' 
-    AND trust_score >= 80
-ORDER BY 
-    distance_km ASC, 
-    trust_score DESC;
+    Bookings
+GROUP BY 
+    user_id
+HAVING 
+    COUNT(*) >= 10 
+    AND SUM(CASE WHEN status = 'CANCELLED' THEN 1 ELSE 0 END) > 5;
